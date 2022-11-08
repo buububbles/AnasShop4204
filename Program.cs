98bd02eac70eb25990e3311393ddd4fe339337df
buububbles <anastasia.parkinson04@gmail.com>
2022-11-08 16:00:03 +1300

    view funds + add funds + cat 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace AnasShop4204
     internal class Program
     {
         public static string? name;
+        public static int balance = 100;
         static void Main(string[] args)
         {   //welcoming message + users input for name
             Console.WriteLine("Hello! Welcome to Ana's Shop.");
@@ -18,7 +20,10 @@ namespace AnasShop4204
         }
 
         public static void Menu()
-        {  //main menu for shop with categories
+
+        {
+            int deposit;
+            //main menu for shop with categories
             Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             Console.WriteLine("Ana's Shop");
             Console.WriteLine("Hello," + name);
@@ -26,15 +31,129 @@ namespace AnasShop4204
             Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             Console.WriteLine("View Balance - [b]");
             Console.WriteLine("Add Funds - [f]");
-            Console.WriteLine("Plush Toys - [p]");
-            Console.WriteLine("Fidget Toys - [t]");
-            Console.WriteLine("Candy - [c]");
-            Console.WriteLine("Shirts - [s]");
-            Console.WriteLine("Accessories - [a]");
-            Console.WriteLine("Previous Purcahses - [p]");
+            Console.WriteLine("Purchase a Plush Toy - [t]");
+            Console.WriteLine("Purchase a Shirt - [s]");
+            Console.WriteLine("Purchase an Accessory - [a]");
             Console.WriteLine("Exit Shop - [e]");
             Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
+            switch (Console.ReadLine())
+            {
+                case "b":
+                    Console.WriteLine("Your current balance is" + " $" + balance);
+                    Menu();
+                    Console.ReadKey();
+                    break;
+
+                case "f":
+                    Console.WriteLine("Please add the number of fund you would like to");
+                    deposit = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Thank you");
+                    balance = balance + deposit;
+                    Menu();
+                    Console.ReadKey();
+                    break;
+                //category one start     
+                case "t":
+                    Console.Clear();
+                    int price1 = 15;
+                    int price2 = 12;
+                    Console.WriteLine("Welcome");
+                    Console.WriteLine("Pick an option below to purchase a Plush Toy");
+                    Console.WriteLine("Cat Plush - " + " $" + price2 + " [c]" );
+                    Console.WriteLine("Dog Plush - " +" $" + price2 + " [d]");
+                    Console.WriteLine("Polar Bear Plush - " +" $"+ price1 +" [p]");
+                    Console.WriteLine("Bee Plush - " + " $" + price2 + " [b]");
+                    Console.WriteLine("Red Panda Plush - "+" $" + price1 +" [r]");
+        
+                    switch (Console.ReadLine())
+                    {
+                        case "c":
+                            if (balance >= price2)
+                            {
+                                Console.WriteLine("Thank you for your purchase!");
+                                balance = balance - price2;
+                                Console.WriteLine("You have brought the Cat Plush, your balance is now" + " $" + balance);
+                                Menu();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, not enough funds ");
+                                Menu();
+                            }
+                            break;
+
+                        case "d":
+                            if (balance >= price2)
+                            {
+                                Console.WriteLine("Thank you for your purchase!");
+                                balance = balance - price2;
+                                Console.WriteLine("You have brought the Dog Plush, your balance is now" + " $" + balance);
+                                Menu();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, not enough funds ");
+                                Menu();
+                            }
+                            break;
+
+                        case "p":
+                            if (balance >= price1)
+                            {
+                                Console.WriteLine("Thank you for your purchase!");
+                                balance = balance - price1;
+                                Console.WriteLine("You have brought the Polar Bear Plush, your balance is now" + " $" + balance);
+                                Menu();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, not enough funds ");
+                                Menu();
+                            }
+                            break;
+
+                        case "b":
+                            if (balance >= price2)
+                            {
+                                Console.WriteLine("Thank you for your purchase!");
+                                balance = balance - price2;
+                                Console.WriteLine("You have brought the Bee Plush, your balance is now" + " $" + balance);
+                                Menu();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, not enough funds ");
+                                Menu();
+                            }
+                            break;
+
+                        case "r":
+                            if (balance >= price1)
+                            {
+                                Console.WriteLine("Thank you for your purchase!");
+                                balance = balance - price1;
+                                Console.WriteLine("You have brought the Red Panda Plush, your balance is now" + " $" + balance);
+                                Menu();
+                            }
+                            else
+                            {
+                                Console.WriteLine("Error, not enough funds ");
+                                Menu();
+                            }
+                            break;
+                    }
+                    
+                    break; //end of category one
+
+             //start of catergory two
+                case "s":
+
+                    break;
+            } 
         }
+
+        
     }
 
 
